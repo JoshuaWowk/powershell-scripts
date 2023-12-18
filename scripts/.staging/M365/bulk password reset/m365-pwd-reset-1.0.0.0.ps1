e85f6d9c31abf0b83c9abe928ff8bfe87ac945d1
Josh Wowk <joshua.wowk@hp-ne.com>
2023-12-18 13:36:47 -0500

    old  updates

---
+++ scripts/.staging/M365/bulk password reset/m365-pwd-reset-1.0.0.0.ps1
@@ -0,0 +1,30 @@
+
+# Define the path to the CSV file containing the list of user UPNs and new passwords
+$csvFilePath = "C:\path\to\userlist.csv"
+
+# Define the path to the CSV file where the results will be logged
+$logFilePath = "C:\path\to\password_reset_log.csv"
+
+# Load the user UPNs and new passwords from the CSV file
+$userData = Import-Csv -Path $csvFilePath
+
+# Loop through each user and change their password
+foreach ($user in $userData) {
+    $upn = $user.UPN
+    $newPassword = $user.NewPassword
+
+    # Change the user's password
+    Set-MsolUserPassword -UserPrincipalName $upn -NewPassword $newPassword
+
+    # Get the user's display name
+    $user = Get-MsolUser -UserPrincipalName $upn
+    $displayName = $user.DisplayName
+
+    # Log the user's display name, UPN, and new password in the CSV file
+    $logEntry = [PSCustomObject]@{
+        DisplayName = $displayName
+        UPN = $upn
+        NewPassword = $newPassword
+    }
+    $logEntry | Export-Csv -Path $logFilePath -Append -NoTypeInformation
+}
---
+++ snippets/update-tacticalRMM-SSL-cert.ps1
@@ -0,0 +1,1 @@
+sudo certbot certonly --manual -d *.hp-ne.us --agree-tos --no-bootstrap --preferred-challenges dns -m joshua.wowk@hp-ne.com --no-eff-email
