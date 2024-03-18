eac8e1eef014fc906e5cd1e93eb48f051b5c0e51
Josh Wowk <joshua.wowk@hp-ne.com>
2024-03-18 09:26:06 -0400

    Refactor kill-and-restart-elgato-audio-server.ps1 script

--- snippets/kill-and-restart-elgato-audio-server.ps1
+++ snippets/kill-and-restart-elgato-audio-server.ps1
@@ -3,22 +3,22 @@ $serverProcessName = "ElgatoAudioControlServer"
 $installationPath = "C:\Program Files\Elgato\Volume Controller"
 $logFilePath = Join-Path -Path $installationPath -ChildPath "scriptLog.txt"
 
-# Check if log file exists, if not create it
-if (-not (Test-Path -Path $logFilePath)) {
-    New-Item -Path $logFilePath -ItemType File
-}
+# # Check if log file exists, if not create it
+# if (-not (Test-Path -Path $logFilePath)) {
+#     New-Item -Path $logFilePath -ItemType File
+# }
 
-# Start transcript and append to log file
-Start-Transcript -Path $logFilePath -Append -Force
+# # Start transcript and append to log file
+# Start-Transcript -Path $logFilePath -Append -Force
 
 # Kill the watcher process
-$watcherProcess = Get-Process -Name $watcherProcessName #-ErrorAction SilentlyContinue
+$watcherProcess = Get-Process -Name $watcherProcessName -ErrorAction SilentlyContinue
 if ($watcherProcess) {
     $watcherProcess | Stop-Process -Force
 }
 
 # Kill the server process
-$serverProcess = Get-Process -Name $serverProcessName #-ErrorAction SilentlyContinue
+$serverProcess = Get-Process -Name $serverProcessName -ErrorAction SilentlyContinue
 if ($serverProcess) {
     $serverProcess | Stop-Process -Force
 }
